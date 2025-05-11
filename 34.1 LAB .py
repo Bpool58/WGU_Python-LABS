6a5c7bda209d6a82fbf2bdde256b5e032a9a76dd
Branson Poole <bpool58@wgu.edu>
2025-05-11 05:49:39 -0700

    Add questions to beginning

--- 34.1 LAB .py
+++ 34.1 LAB .py
@@ -1,3 +1,30 @@
+"""
+Instructions:
+Create a Python solution to the following task.
+Ensure that the solution produces output in exactly the same format shown in the sample(s) below,
+ including capitalization and whitespace.
+
+Task:
+Create a solution that accepts three integer inputs representing the number of times an employee travels to a job site.
+Output the total distance traveled to two decimal places given the following miles per employee commute to the job site.
+Output the total distance traveled to two decimal places given the following miles per employee commute to the job site:
+
+Employee A: 15.62 miles
+Employee B: 41.85 miles
+Employee C: 32.67 miles
+The solution output should be in the format
+
+Distance: total_miles_traveled miles
+Sample Input/Output:
+If the input is
+
+1
+2
+3
+then the expected output is
+
+Distance: 197.33 miles
+"""
 # Constants for miles per trip
 EMPLOYEE_A_MILES = 15.62
 EMPLOYEE_B_MILES = 41.85
--- 34.7 LAB .py
+++ 34.7 LAB .py
@@ -1,3 +1,28 @@
+"""
+Instructions:
+Create a Python solution to the following task.
+Ensure that the solution produces output in exactly the same format shown in the sample(s) below,
+ including capitalization and whitespace.
+
+Task:
+Create a solution that accepts an integer input to compare against the following list:
+
+predef_list = [4, -27, 15, 33, -10]
+
+Output a Boolean value indicating whether the input value is greater than the maximum value from predef_list
+
+The solution output should be in the format
+
+Greater Than Max? Boolean_value
+Sample Input/Output:
+If the input is
+
+20
+then the expected output is
+
+Greater Than Max? False
+
+"""
 # Define the predetermined list of numbers
 predef_list = [4, -27, 15, 33, -10]
 
