37d5e0bad100a41af464634df781b6d9644ed63b
Branson Poole <bpool58@wgu.edu>
2025-05-24 23:19:51 -0700

    lists and loop for stock count

---
+++ Q9 PreAssessment.py
@@ -0,0 +1,8 @@
+
+
+num_stocks = int(input())
+
+
+stocks = {'TSLA': 912.86 , 'BBBY': 24.84, 'AAPL': 174.26, 'SOFI': 6.92, 'KIRK': 8.72, 'AURA': 22.12, 'AMZN': 141.28, 'EMBK': 12.29, 'LVLU': 2.33}
+
+
