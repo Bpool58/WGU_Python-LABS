632b63374a7dafcbdd3b8b30e405e08da6695ef0
Branson Poole <bpool58@wgu.edu>
2025-06-07 05:29:57 -0700

    Add Mad Libs sentence generator with quit functionality

--- 10.8 Lab .py
+++ 10.8 Lab .py
@@ -1,5 +1,5 @@
 """
-Mad Libs are activities that have a person provide various words,
+Mad Libs are activities that have a person provide various words.
  which are then used to complete a short story in unexpected (and hopefully funny) ways.
 - Write a program that takes a string and an integer as input, and outputs a sentence using
 the input values as shown in the example below. The program repeats until the input string is
