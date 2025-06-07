fcb1ad4e27ac89c4690bd62e66b0d1dec4969f68
Branson Poole <bpool58@wgu.edu>
2025-06-07 05:47:24 -0700

    Create pattern printer using base and head characters

--- 33.5 LAB .py
+++ 33.5 LAB .py
@@ -1,6 +1,6 @@
 """
 Given four values representing counts of quarters, dimes,
- nickels and pennies, output the total amount as dollars and cents.
+ nickels and pennies, output the total amount as dollars and cents..
 
 Output each floating-point value with two digits after the decimal point,
 which can be achieved as follows:
