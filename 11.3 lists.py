da75afc25768ea094dce831eb0f7473bb963c463
Branson Poole <bpool58@wgu.edu>
2025-06-11 23:05:07 -0700

    Iterating through a List

---
+++ 11.3 lists.py
@@ -0,0 +1,24 @@
+
+user_input = input("Enter numbers: ")
+
+tokens = user_input.split()
+
+nums = []
+
+for token in tokens:
+    nums.append(int(token))
+
+for index in range(len(nums)):
+    value = nums[index]
+    print(f"{index}: {value}")
+
+max_num = None
+for num in nums:
+    if (max_num == None) and (num % 2 == 0):
+        max_num = num
+
+    elif (num > max_num) and (max_num % 2 == 0):
+        max_num = num
+print(max_num)
+
+print(nums)
