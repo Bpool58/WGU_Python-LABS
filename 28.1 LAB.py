f13c405f56c9455a865b75a14bca51267a94da3a
Branson Poole <bpool58@wgu.edu>
2025-06-28 05:05:55 -0700

    Find the middle number in a list of sorted integers. Methods used: split, //, len, if/else

---
+++ 28.1 LAB.py
@@ -0,0 +1,10 @@
+"""
+Given a sorted list of integers, find the middle element.
+"""
+numbers = input('Enter numbers: ').split()
+print(numbers)
+
+middle_num = numbers[len(numbers)//2]
+
+print(middle_num)
+
